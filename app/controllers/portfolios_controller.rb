4744176d5356a2c62ed08c102e877e59b2731e6b
atitlanretreats <centroatitlan@gmail.com>
2018-03-07 16:43:28 -0600

    portfolio#edit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,16 +8,34 @@ class PortfoliosController < ApplicationController
     end
     
     def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
-      else
-        format.html { render :new }
-        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
-      end
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to portfolios_path, notice: 'portfolio was successfully created.' }
+            format.json { render :show, status: :created, location: @portfolio_item }
+          else
+            format.html { render :new }
+            format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+          end
+        end
     end
-  end
+
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+        
+        respond_to do |format|
+          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfolios_path, notice: 'portfolio was successfully updated.' }
+            format.json { render :show, status: :ok, location: @portfolio_item }
+          else
+            format.html { render :edit }
+            format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+          end
+        end
+    end
 end
