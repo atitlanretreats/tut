a48c98c2dbc5133654a91d1ef5588d539ef3c582
atitlanretreats <centroatitlan@gmail.com>
2018-03-07 17:40:39 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
+    
+  root to: 'pages#home'
 
-  get 'pages/contac'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'  
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
