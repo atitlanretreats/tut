4f1358865f25f75cd0501af18ba2c6aba5ec10c6
atitlanretreats <centroatitlan@gmail.com>
2018-03-12 00:45:46 -0600

    controllers

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,6 +5,7 @@ class PortfoliosController < ApplicationController
 
     def new
         @portfolio_item = Portfolio.new
+        3.times {@portfolio_item.technologies.build}
     end
 
     def show
@@ -12,7 +13,7 @@ class PortfoliosController < ApplicationController
     end
 
     def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        @portfolio_item = Portfolio.new(portfolio_params)
 
         respond_to do |format|
           if @portfolio_item.save
@@ -33,7 +34,7 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.find(params[:id])
 
         respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+          if @portfolio_item.update(portfolio_params)
             format.html { redirect_to portfolios_path, notice: 'portfolio was successfully updated.' }
             format.json { render :show, status: :ok, location: @portfolio_item }
           else
@@ -51,5 +52,11 @@ class PortfoliosController < ApplicationController
           format.html { redirect_to portfolios_url, notice: 'Item was successfully destroyed.' }
           format.json { head :no_content }
         end
+    end
+
+    private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
     end
 end
