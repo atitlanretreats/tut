5b75b43edfdb876604dc110dc862c561f8fc906f
atitlanretreats <centroatitlan@gmail.com>
2018-03-14 14:18:46 -0600

    views stuff

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
     layout 'portfolio'
     def index
         @portfolio_items = Portfolio.all
@@ -10,11 +11,9 @@ class PortfoliosController < ApplicationController
     end
 
     def show
-        @portfolio_item = Portfolio.find(params[:id])
     end
 
     def create
-        @portfolio_item = Portfolio.new(portfolio_params)
 
         respond_to do |format|
           if @portfolio_item.save
@@ -28,12 +27,9 @@ class PortfoliosController < ApplicationController
     end
 
     def edit
-        @portfolio_item = Portfolio.find(params[:id])
     end
 
     def update
-        @portfolio_item = Portfolio.find(params[:id])
-
         respond_to do |format|
           if @portfolio_item.update(portfolio_params)
             format.html { redirect_to portfolios_path, notice: 'portfolio was successfully updated.' }
@@ -46,8 +42,6 @@ class PortfoliosController < ApplicationController
     end
 
     def destroy
-        @portfolio_item = Portfolio.find(params[:id])
-
         @portfolio_item.destroy
         respond_to do |format|
           format.html { redirect_to portfolios_url, notice: 'Item was successfully destroyed.' }
@@ -59,5 +53,9 @@ class PortfoliosController < ApplicationController
 
     def portfolio_params
       params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+    end
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
     end
 end
